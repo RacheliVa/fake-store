b492d1a0fa6dafa297138062030a1c998aadafb1
RacheliVa <racheli.litman@gmail.com>
2024-10-28 13:28:11 +0200

    desigh_prudact

--- src/component/Jewelry/Jewelry.jsx
+++ src/component/Jewelry/Jewelry.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getJewelry } from "../../services/service";
 import Product  from "../Product/Product";
+import styles from './Jewelry.module.css'
 
 const Jewelry = () => {
     const [items, setItems] = useState([]);
@@ -16,7 +17,7 @@ const Jewelry = () => {
     return (
         <div>
             <h2>Jewelry</h2>
-            <div >
+            <div className={styles.container}>
                 {items.map(item => 
                     <Product key={item.id} product={item} /> 
                 )}
--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -3,14 +3,14 @@ import styles from './Product.module.css'
 
 const Product = ({ product }) => {
     return (
-        <div className="card">
-            <img src={product.image} alt={product.title} className="styles.image" />
-            <div className="styles.content">
+        <div className={styles.card}>
+            <img src={product.image} alt={product.title} className={styles.image} />
+            <div className={styles.content}>
                 <h2>{product.title}</h2>
-                <p><strong>Price:</strong> ${product.price}</p>
+                <p className={styles.price}><strong>Price:</strong> ${product.price.toFixed(2)}</p>
                 <p><strong>Category:</strong> {product.category}</p>
                 <p>{product.description}</p>
-                <div className="styles.rating">
+                <div className={styles.rating}>
                     <p><strong>Rating:</strong> {product.rating.rate} / 5</p>
                     <p><strong>Reviews:</strong> {product.rating.count}</p>
                 </div>
